59fd13784d0fe59f0e5bb668e033ace5250567c3
iwan <1322677864@qq.com>
2024-11-23 22:06:45 +0800

    fix

--- frame.js
+++ frame.js
@@ -12,11 +12,11 @@ const ananlyeVideo = async (filepath) => {
 
   let list = [];
 
-  for (let item of video_info.slice(0, 3)) {
+  for (let item of video_info.slice(0, 1)) {
     const base64_image = readFileSync(item.outputFile, "base64");
     const image_desc = await ananlyeVideoItem(base64_image);
 
-    console.log("处理图片", item.outputFile,item.timestamp);
+    console.log("处理图片", item.outputFile, item.timestamp);
 
     list.push({
       ...item,
@@ -25,49 +25,51 @@ const ananlyeVideo = async (filepath) => {
   }
   const script = await createScript(list);
 
-
-
   let input_text = script.script_info.content;
-
 
   writeFileSync("./script.txt", script.script_info.content, "utf-8");
 
-  let result_list=[]
- try{
-    const info=JSON.parse(input_text)
+  let result_list = [];
+  try {
+    const info = JSON.parse(input_text);
+    console.log({
+      ...info,
+      script
+    });
 
-    if(info.messages && Array.isArray(info.messages)){
-        const d=info.messages[0]
-        if(typeof d.content === "string"){
-            console.log(d.content)
-            const s=JSON.parse(d.content)
-            result_list=s;
-        }else {
-            if(Array.isArray(d.content)){
-                result_list=d.content;
-            }
-            console.log(d.content,'con')
-        }
+    if(Array.isArray(info)){
+        result_list = content;
     }
- }catch(e){
-    let r=input_text;
-    const fixedContent = r.replace(/\\"/g, '"');
-    console.log(fixedContent)
-    const contentArray = JSON.parse(fixedContent);
-    console.log(contentArray);
-    console.log("json解析错误",{
-        r
-    },e)
- }
 
- return result_list.map((v,index)=>{
-    const cover=video_info[index]?.outputFile;
+    if (info && info.messages && Array.isArray(info.messages)) {
+      const content = info.messages[0].content;
+      if (Array.isArray(content)) {
+        result_list = content;
+      }
+
+      if(typeof content == "string"){
+        result_list=JSON.parse(content)
+      }
+    }
+  } catch (e) {
+    let r = input_text;
+
+    console.log(
+      {
+        r,
+        script,
+      },
+      "json 解析出错"
+    );
+  }
+
+  return result_list.map((v, index) => {
+    const cover = video_info[index]?.outputFile;
     return {
-        ...v,
-        cover
-    }
- })
-
+      ...v,
+      cover,
+    };
+  });
 };
 
 module.exports = {
